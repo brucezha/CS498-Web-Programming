9eb4089685c07783cfb5131671cdb2c0427e5d93
brucezha <jahethan@gmail.com>
2017-11-16 14:12:15 -0500

    Add files via upload

---
+++ mp3/source_js/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('mp3',['ngRoute']);
+
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/list.html',
+            controller: 'listController'
+        })
+        .when('/gallery', {
+            templateUrl: 'partials/gallery.html',
+            controller: 'galleryController'
+        })
+        .when('/details', {
+            templateUrl: 'partials/details.html',
+            controller: 'detailsController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
